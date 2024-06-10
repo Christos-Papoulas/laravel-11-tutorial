eb24f9776fa8fd461bb74eec8cad0eb870f4141f
Christos Papoulas <pachristos@ix.gr>
2024-06-10 18:16:08 +0300

    Day 28: Blade & tailwind, continue UI

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<x-panel class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-800 group transition-colors duration-300">
+    <div>
+        <x-employer-logo />
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a class="self-start text-sm text-gray-400">Laracasts</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full time - From $60,000</p>
+    </div>
+
+    <div class="space-x-1">
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</x-panel>
--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,3 +1,15 @@
-<a class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">
+@props(['size' => 'base'])
+
+@php
+    $classes = 'bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300';
+    if ($size === 'base') {
+        $classes .= ' px-5 py-1 text-sm';
+    }
+
+    if ($size === 'small') {
+        $classes .= ' px-2 py-1 text-xs';
+    }
+@endphp
+<a {{ $attributes }} class="{{ $classes }}">
     {{ $slot }}
 </a>
